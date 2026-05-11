397ba40e651558ff2bb43c939558a0ca74bdbaef
Shaik Gafoor <gafoor7898@gmail.com>
2026-05-11 14:47:30 +0530

    included the Request for fine and updated the fineservice

--- src/main/java/com/example/demo/Services/FineService.java
+++ src/main/java/com/example/demo/Services/FineService.java
@@ -1,8 +1,30 @@
 package com.example.demo.Services;
 
+import com.example.demo.domain.FineStatus;
+import com.example.demo.domain.FineType;
 import com.example.demo.payload.dto.FineDTO;
 import com.example.demo.payload.request.CreateFineRequest;
+import com.example.demo.payload.request.WaiveFineRequest;
+import com.example.demo.payload.response.PaymentInitiateResponse;
+import com.example.demo.payload.response.pageResponse;
 
-public class FineService {
+import java.util.List;
+
+public interface FineService {
     FineDTO createFine(CreateFineRequest createFineRequest);
+    PaymentInitiateResponse payFine(Long fineId, String transactionId);
+
+    void markFineAsPaid(Long fineId, Long amount, String transactionId);
+
+    FineDTO waiveFine(WaiveFineRequest waiveFineRequest);
+
+    List<FineDTO> getMyFines(FineStatus status, FineType type);
+
+    pageResponse<FineDTO> getAllFines(
+            FineStatus status,
+            FineType type,
+            Long userId,
+            int page,
+            int size
+    );
 }
---
+++ src/main/java/com/example/demo/payload/request/WaiveFineRequest.java
@@ -0,0 +1,21 @@
+package com.example.demo.payload.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WaiveFineRequest {
+    @NotNull(message = "Fine ID is mandatory")
+    private Long fineId;
+
+    @NotBlank(message = "Waiver reason is mandatory")
+    private String reason;
+}
