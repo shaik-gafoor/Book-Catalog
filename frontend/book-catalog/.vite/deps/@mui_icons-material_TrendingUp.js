03f7087d45f7c143ff154d98a6822bf74d9cf4ee
Shaik Gafoor <gafoor7898@gmail.com>
2026-05-15 15:46:43 +0530

    included the current loans feature in the web

---
+++ frontend/book-catalog/.vite/deps/@mui_icons-material_TrendingUp.js
@@ -0,0 +1,7 @@
+"use client";
+import { n as require_jsx_runtime, t as createSvgIcon } from "./createSvgIcon-Bcc5dSzW.js";
+var TrendingUp_default = createSvgIcon(/* @__PURE__ */ (0, require_jsx_runtime().jsx)("path", { d: "m16 6 2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6z" }), "TrendingUp");
+//#endregion
+export { TrendingUp_default as default };
+
+//# sourceMappingURL=@mui_icons-material_TrendingUp.js.map
---
+++ frontend/book-catalog/src/pages/Dashboard/ReadingHistory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ReadingHistory() {
+  return <div></div>;
+}
+
+export default ReadingHistory;
