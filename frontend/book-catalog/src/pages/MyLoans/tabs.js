40d49c66f3dd9347def83f57d0c411c18304aeb6
Shaik Gafoor <gafoor7898@gmail.com>
2026-05-19 16:38:59 +0530

    included the myloans feature into the frontend web

---
+++ frontend/book-catalog/src/pages/MyLoans/tabs.js
@@ -0,0 +1,8 @@
+export const tabs = [
+  { label: "All", value: null },
+  { label: "Active", value: "ACTIVE" },
+  { label: "Overdue", value: "OVERDUE" },
+  { label: "Returned", value: "RETURNED" },
+  { label: "Lost", value: "LOST" },
+  { label: "Damaged", value: "DAMAGED" },
+];
